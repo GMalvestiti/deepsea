1f176f700ee5877a0ea3381cad4c5abd0a553724
Gustavo Malvestiti <101594283+GMalvestiti@users.noreply.github.com>
2026-03-24 23:45:14 -0300

    [feat] port minecraft 26.1 (#23)
    * [feat] update 26.1
    
    * [fix] fix build script

--- src/main/java/net/riser876/deepsea/datagen/provider/DeepSeaEntityTypeTagProvider.java
+++ src/main/java/net/riser876/deepsea/datagen/provider/DeepSeaEntityTypeTagProvider.java
@@ -1,21 +1,21 @@
 package net.riser876.deepsea.datagen.provider;
 
-import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
-import net.fabricmc.fabric.api.datagen.v1.provider.FabricTagProvider;
+import net.fabricmc.fabric.api.datagen.v1.FabricPackOutput;
+import net.fabricmc.fabric.api.datagen.v1.provider.FabricTagsProvider;
 import net.fabricmc.fabric.api.tag.convention.v2.ConventionalEntityTypeTags;
 import net.minecraft.core.HolderLookup;
 import net.riser876.deepsea.registry.DeepSeaTags;
 
 import java.util.concurrent.CompletableFuture;
 
-public class DeepSeaEntityTypeTagProvider extends FabricTagProvider.EntityTypeTagProvider {
+public class DeepSeaEntityTypeTagProvider extends FabricTagsProvider.EntityTypeTagsProvider {
 
-    public DeepSeaEntityTypeTagProvider(FabricDataOutput output, CompletableFuture<HolderLookup.Provider> completableFuture) {
+    public DeepSeaEntityTypeTagProvider(FabricPackOutput output, CompletableFuture<HolderLookup.Provider> completableFuture) {
         super(output, completableFuture);
     }
 
     @Override
-    protected void addTags(HolderLookup.Provider wrapperLookup) {
+    protected void addTags(HolderLookup.Provider registries) {
         builder(DeepSeaTags.DEEP_SEA_BOAT).addOptionalTag(ConventionalEntityTypeTags.BOATS);
     }
 }
